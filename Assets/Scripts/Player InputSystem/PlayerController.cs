193b7614407ad51a5ee8fd9ea5096966d827a280
Kevvv <120093229+1015lhw@users.noreply.github.com>
2026-02-16 02:53:25 +0000

    Movement Sys added

---
+++ Assets/Scripts/Player InputSystem/PlayerController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 6f;
+    public float rotationSpeed = 15f;
+
+    private Rigidbody rb;
+    private Vector2 moveInput;
+
+    private PlayerInputActions inputActions;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        inputActions = new PlayerInputActions();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Enable();
+        inputActions.Player.Move.performed += OnMove;
+        inputActions.Player.Move.canceled += OnMove;
+    }
+
+    void OnDisable()
+    {
+        inputActions.Player.Move.performed -= OnMove;
+        inputActions.Player.Move.canceled -= OnMove;
+        inputActions.Disable();
+    }
+
+    void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate()
+    {
+        // 固定相机 → 用世界坐标
+        Vector3 move = new Vector3(-moveInput.x, 0f, -moveInput.y);
+
+        if (move.sqrMagnitude > 0.01f)
+        {
+            rb.MovePosition(rb.position + move * moveSpeed * Time.fixedDeltaTime);
+
+            Quaternion targetRotation = Quaternion.LookRotation(move);
+            rb.rotation = Quaternion.Slerp(rb.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
