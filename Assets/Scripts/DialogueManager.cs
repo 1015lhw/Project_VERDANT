4d0a5c0b3c45494fb82d61aa82d845f5d97a09f7
Good Hunter <lakeland.irys@gmail.com>
2026-02-26 04:49:27 +0000

    Choice Display Fixed

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -3,6 +3,8 @@ using Ink.Runtime;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -17,7 +19,26 @@ public class DialogueManager : MonoBehaviour
 
     private Ink.Runtime.Story story;
     private bool isOpen;
+    bool ClickedOnChoiceButton()
+    {
+        if (EventSystem.current == null) return false;
 
+        var pointer = new PointerEventData(EventSystem.current)
+        {
+            position = Input.mousePosition
+        };
+
+        var results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointer, results);
+
+        foreach (var r in results)
+        {
+            if (choicesContainer != null && r.gameObject.transform.IsChildOf(choicesContainer))
+                return true;
+        }
+
+        return false;
+    }
     void Awake()
     {
         Instance = this;
@@ -27,12 +48,16 @@ public class DialogueManager : MonoBehaviour
     {
         if (!isOpen) return;
 
-        // if there are choices, don't continue the dialogue by space
+        //if there are choices, dont continue
         if (story != null && story.currentChoices != null && story.currentChoices.Count > 0)
             return;
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
+            //only when you click on choices button, it won't continue. Finally fixed I hate this stupid coding stuff
+            if (ClickedOnChoiceButton())
+                return;
+
             ContinueStory();
         }
     }
